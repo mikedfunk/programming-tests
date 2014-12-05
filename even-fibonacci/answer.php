534e76d40c56d12b1ca2f330e5c57450756cadde
Mike Funk <mike@mikefunk.com>
2014-12-04 23:19:03 -0800

    working

--- even-fibonacci/answer.php
+++ even-fibonacci/answer.php
@@ -4,17 +4,19 @@
 $fibonacciNumbers = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
 
 // loop through all numbers from 90 to 4 million
-
 $i = 90;
 while ($i <= 4000000) {
     $lastTwo = array_slice($fibonacciNumbers, -2, 2);
     $fibonacciNumbers[] = $i = array_sum($lastTwo);
 }
 
+// filter down to just the even ones
 $evenFibonacciNumbers = array_filter(
     $fibonacciNumbers,
     function ($number) {
         return $number %2 === 0;
     }
 );
+
+// echo the sum
 var_dump(array_sum($evenFibonacciNumbers));
